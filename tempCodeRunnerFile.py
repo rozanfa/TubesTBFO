1d907cb2c91362cdc9f04d1e2af8ec5b60b7b86a
Timothy Stanley <79507886+Stanley77-web@users.noreply.github.com>
2021-11-22 00:32:54 +0700

    update converter

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+                # grammar = product[0]
+                # if len(product) == 1 and isParam(grammar):
+                #     products.remove(product)
+                #     newProduct = dc([product for product in CFG[product[0]] if product not in products])
+                #     products.extend(newProduct)
+                #     clear = False
+                #     break
