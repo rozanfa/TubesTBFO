71697680fea7991ede66a0e77ffb9cd153e5b977
BDS_Hyper <haqibariza@gmail.com>
2021-11-22 18:04:47 +0700

    update error element kosong

--- CYKAlgorithm.py
+++ CYKAlgorithm.py
@@ -1,5 +1,3 @@
-from printcolors import colors
-
 def CYKAlgorithm(convertedCodeInput,codeInput,CNF):
     m = len(CNF)
     n = len(convertedCodeInput)
@@ -15,22 +13,20 @@ def CYKAlgorithm(convertedCodeInput,codeInput,CNF):
     table = [[[0 for i in range(m + 1)] for j in range(n + 1)] for k in range(n + 1)]
     keyword = {}
     data = [None] * (m + 1)
-
     # Set dictionary convertedCodeInput dan mengambil data dari CNF
     count=0
     for key in CNF:
         data[count + 1] = CNF[key]
         keyword[key] = count + 1
         count+=1
-
     # Algoritmanya
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             for element in data[j]:
-                if len(element) != 0:
-                    if (element[0] == convertedCodeInput[i - 1]):
-                        table[1][i][j] = True #input terminal awal pada tabel
-                        break
+                if (element[0] == convertedCodeInput[i - 1]):
+                    table[1][i][j] = True #input terminal awal pada tabel
+                    break
+                
 
     for i in range(2, n + 1):
         for j in range(1, (n - i + 2)):
@@ -38,8 +34,7 @@ def CYKAlgorithm(convertedCodeInput,codeInput,CNF):
                 for l in range(1, m + 1):
                     for element in data[l]:
                         # print("elmt :", element)
-                        if (len(element) > 1 and element is not str): #jika elemen merupakan variabel
-                            
+                        if (len(element)!=1): #jika elemen merupakan variabel
                             var1 = keyword[element[0]]
                             var2 = keyword[element[1]]
                             if (table[k][j][var1] and table[i - k][j + k][var2]):
@@ -50,13 +45,10 @@ def CYKAlgorithm(convertedCodeInput,codeInput,CNF):
     if (table[n][1][1]): #jika tabel akhir bernilai True
         print("Congratulations! Your code is accepted")
         print("Your code :")
-        print(colors.HEADER + "-----------------------------------------" + colors.HEADER )
-        print(colors.OKGREEN, end = "")
+        print("-----------------------------------------")
         for i in range(len(listLines)):
-            print(str(i+1).rjust(3) + ' | ' + listLines[i])
-        print(colors.OKGREEN, end = "")
-        print(colors.HEADER + "-----------------------------------------" + colors.HEADER )
-        print(colors.OKBLUE + "ACCEPTED" + colors.ENDC)
+            print(listLines[i])
+        print("-----------------------------------------")
     else :
         index = 1
         for i in range(n, 0, -1):
@@ -64,15 +56,8 @@ def CYKAlgorithm(convertedCodeInput,codeInput,CNF):
                 break
             elif (convertedCodeInput[i - 1] == '\n'):
                 index = endLines[i - 1]
-        print("Sorry, your code is not accepted")
-        print("Your code :")
-        print(colors.HEADER + "-----------------------------------------" + colors.HEADER )
-        print(colors.WARNING, end = "")
         for i in range(len(listLines)):
             if (i==index-1):
-                print(str(i+1).rjust(3) + ' | ' + listLines[i]+ colors.FAIL + "    <-- Error here" + colors.WARNING)
+                print(listLines[i]+"    <-- Error line "+str(index))
             else:
-                print(str(i+1).rjust(3) + ' | ' + listLines[i])
-        print(colors.ENDC, end = "")
-        print(colors.HEADER + "-----------------------------------------" + colors.HEADER )
-        print(colors.FAIL + "Error in line 1" + colors.ENDC)
+                print(listLines[i])
