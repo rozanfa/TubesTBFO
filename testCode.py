344901f3467d8d040ac1434bd43ff01653c70218
rozanfa <16520141@std.stei.itb.ac.id>
2021-11-23 12:26:05 +0700

    update cfg

--- testCode.py
+++ testCode.py
@@ -1,13 +1,8 @@
-def do_something(x):
-    ''' This is a sample multiline comment
-    '''
-    x + 2 = 3
-    if x == 0 + 1
-        return 0
-    elif x + 4 == 1:
-        else:
-            return 2
-    elif x == 32:
-        return 4
-    else:
-        return "Doodoo"
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
