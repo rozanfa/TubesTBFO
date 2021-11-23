7202792425b31c8373536b32a0ee9043df6a4a48
rozanfa <16520141@std.stei.itb.ac.id>
2021-11-23 12:35:32 +0700

    tambah cfg

--- testCode.py
+++ testCode.py
@@ -1,8 +1,31 @@
-import matplotlib.pyplot as plt
-import numpy as np
+#NIM/Nama  : 16621251/Hana Nadia Iskandar
+#Tanggal   : 5 November 2021
+#Deskripsi : Problem 2
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
+#KAMUS
+#N, P, i, p, o: integer
+#arr: array of integers range N
 
-plt.plot(xpoints, ypoints)
-plt.show()
+#ALGORTIMA
+#inisiasi
+N=int(input("Masukkan jumlah hari (N): "))
+arr=[0 for i in range (N)]
+
+#Input array
+for i in range (N):
+    arr[i]=int(input("Masukkan elemen ke "+str(i+1)+": "))
+
+#input pergeseran P
+P=int(input("Masukkan P: "))
+
+#proses dan output
+for p in range(N-P,N-1): 
+    print("Array setelah digeser adalah [",arr[p], end=" ")
+for o in range(-1, N-P):
+    print(arr[o], end=" ")
+print("].")
+    
+
+
+
+
