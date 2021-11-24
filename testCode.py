4365dff031119c04df0ae3e7203f98571b6f038a
rozanfa <16520141@std.stei.itb.ac.id>
2021-11-24 11:05:40 +0700

    benerin grammar lagi

--- testCode.py
+++ testCode.py
@@ -1,1 +1,6 @@
-a, b, c = getNumber()
+def findGadgetByYear(year, operator, arr=[1,2,3]):
+
+    for i in range(1,n):
+        if operator == '=':
+            if int(arr[i][5]) == year:
+                printGroup(i, arr)
